8827c461f537f479e283e22b3bdb640d426f9b0c
Allen Ladd <ghost2130@gmail.com>
2021-09-25 23:16:04 -0400

    added resume tab and resume link

--- REACT_app/01-app/src/components/pages/Resume.js
+++ REACT_app/01-app/src/components/pages/Resume.js
@@ -9,15 +9,24 @@ function Resume() {
           Download Resume
         </Nav.Link>
       </Nav>
-
+      <h2>Front-End</h2>
       <ListGroup variant="flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item href="https://drive.google.com/uc?export=download&id=1ym11KiC5FsAlLmcc8h9bE8sZ7zgEYKQn">
-          Download Resume
-        </ListGroup.Item>
+        <ListGroup.Item>HTML</ListGroup.Item>
+        <ListGroup.Item>CSS</ListGroup.Item>
+        <ListGroup.Item>JavaScript</ListGroup.Item>
+        <ListGroup.Item>JQuery</ListGroup.Item>
+        <ListGroup.Item>responsive design</ListGroup.Item>
+        <ListGroup.Item>Bootstrap</ListGroup.Item>
+      </ListGroup>
+      <h2>Back-End</h2>
+      <ListGroup variant="flush">
+        <ListGroup.Item>APIs</ListGroup.Item>
+        <ListGroup.Item>Node</ListGroup.Item>
+        <ListGroup.Item>Express</ListGroup.Item>
+        <ListGroup.Item>MySQL, Sequelize</ListGroup.Item>
+        <ListGroup.Item>MongoDB, Mongoose</ListGroup.Item>
+        <ListGroup.Item>REST</ListGroup.Item>
+        <ListGroup.Item>GraphQL</ListGroup.Item>
       </ListGroup>
     </div>
   );
